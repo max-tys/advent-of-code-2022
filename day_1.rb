d4f446bc3edfb341a34d3275675a3e7896589af5
Max Tan <yenseow@gmail.com>
2022-12-01 22:17:43 -0500

    day 1

---
+++ day_1.rb
@@ -0,0 +1,14 @@
+# convert raw input into array of strings (or 'sacks')
+# each sack contains the calorific values of the snacks
+sacks = File.read('day_1_data.txt').split("\n\n")
+
+# transform each string into an integer sum of the calorific values
+sacks.map! do |sack|
+  sack.split("\n")
+      .map(&:to_i)
+      .sum
+end
+
+puts "There are #{sacks.size} elves."
+puts "The greatest number of calories carried by an elf is #{sacks.max}."
+puts "The number of calories carried by the top three elves is #{sacks.max(3).sum}."
