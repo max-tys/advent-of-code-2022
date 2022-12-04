5b85277397ca9eb01b218e665d5b9a8ce22ae24f
Max Tan <yenseow@gmail.com>
2022-12-04 11:18:59 -0500

    day 4

--- day_4.rb
+++ day_4.rb
@@ -1,31 +1,28 @@
-# convert string range into arrays
-# e.g. "2-4" -> [2, 3, 4]
-def range_to_array(range)
-  first, second = range.split('-').map(&:to_i)
-  (first..second).to_a
+# Converts a '-' delimited string range into a Range object
+def string_to_range(str)
+  n1, n2 = str.split('-').map(&:to_i)
+  Range.new(n1, n2)
 end
 
 # checks if any range in a pair is fully contained by the other range
-def contained?(pair)
-  r1 = range_to_array(pair.first)
-  r2 = range_to_array(pair.last)
-
-  (r1 - r2).empty? || (r2 - r1).empty?
+def contained?(ranges)
+  r1, r2 = ranges.first, ranges.last
+  r1.cover?(r2) || r2.cover?(r1)
 end
 
 # checks if the ranges in a pair overlap each other
 # if there's no overlap, Array#& returns an empty array
-def overlapped?(pair)
-  r1 = range_to_array(pair.first)
-  r2 = range_to_array(pair.last)
-
-  !(r1 & r2).empty?
+def overlapped?(ranges)
+  r1, r2 = ranges.first, ranges.last
+  ! (r1.last < r2.first || r1.first > r2.last)
 end
 
-# convert raw input into array of arrays
-# e.g. 83-83,45-82 -> [[83-83], [45-82]]
+# convert raw input into array of array of ranges
+# e.g. "83-83,45-82" -> ['83-83', '45-82'] -> ['83'..'83', '45'..'82'] 
 pairs = File.read('input/day_4.txt').split("\n")
-pairs.map! { |pair| pair.split(',')}
+
+pairs.map! { |pair| pair.split(',') }
+     .map! { |pair| [string_to_range(pair.first), string_to_range(pair.last)] }
 
 # part 1
 contained = pairs.count { |pair| contained? pair }
