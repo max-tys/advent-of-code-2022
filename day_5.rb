48e9d59f7a07c26b43eec9f0c6d3ae833d7ca32b
Max Tan <yenseow@gmail.com>
2022-12-05 09:48:40 -0500

    day 5 (revised)

--- day_5.rb
+++ day_5.rb
@@ -1,18 +1,6 @@
-# Parse the stack input
-def get_stacks
-  stack_input = <<~STACKS
-  [N] [G]                     [Q]    
-  [H] [B]         [B] [R]     [H]    
-  [S] [N]     [Q] [M] [T]     [Z]    
-  [J] [T]     [R] [V] [H]     [R] [S]
-  [F] [Q]     [W] [T] [V] [J] [V] [M]
-  [W] [P] [V] [S] [F] [B] [Q] [J] [H]
-  [T] [R] [Q] [B] [D] [D] [B] [N] [N]
-  [D] [H] [L] [N] [N] [M] [D] [D] [B]
-   1   2   3   4   5   6   7   8   9 
-  STACKS
-
-  lines = stack_input.split("\n").map(&:chars)
+# Parse the stacks drawing
+def parse_stacks(stack_input)
+  lines = stack_input.map(&:chars)
 
   stacks = Hash.new { |hash, key| hash[key] = [] }
 
@@ -27,7 +15,6 @@ def get_stacks
 end
 
 # Part 1: CraneMover 9000 moves crates one at a time
-
 def move_singly!(move, stacks)
   move = move.scan(/(\d+)/).flatten.map(&:to_i)
   
@@ -37,15 +24,14 @@ def move_singly!(move, stacks)
   end
 end
 
-moves = File.read('input/day_5.txt')
-stacks = get_stacks
-moves.each_line { |move| move_singly!(move, stacks) }
+stacks, moves = File.read('input/day_5.txt').split("\n\n").map { |x| x.split("\n") }
+stacks = parse_stacks(stacks)
+moves.each { |move| move_singly!(move, stacks) }
 
 top_stack = (1..9).each_with_object([]) { |n, arr| arr << stacks[n].first}.join
 puts "Part 1: When using CrateMover 9000, the crates on top of each stack are '#{top_stack}'."
 
 # Part 2: CraneMover 9001 moves multiple crates at once
-
 def move_together!(move, stacks)
   move = move.scan(/(\d+)/).flatten.map(&:to_i)
   
@@ -54,9 +40,9 @@ def move_together!(move, stacks)
   stacks[move[2]].flatten!
 end
 
-moves = File.read('input/day_5.txt')
-stacks = get_stacks
-moves.each_line { |move| move_together!(move, stacks) }
+stacks, moves = File.read('input/day_5.txt').split("\n\n").map { |x| x.split("\n") }
+stacks = parse_stacks(stacks)
+moves.each { |move| move_together!(move, stacks) }
 
 top_stack = (1..9).each_with_object([]) { |n, arr| arr << stacks[n].first}.join
 puts "Part 2: When using CrateMover 9001, the crates on top of each stack are '#{top_stack}'."
